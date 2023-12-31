f93b76b687466f43b10f7109863330fb407e0ab5
Arielle Fox <118761163+ArielleFox@users.noreply.github.com>
2023-12-31 14:52:44 +0100

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,9 @@
+clean
+echo "Compiling Now..."
+clang ./montag.c -o montag
+clang ./dienstag.c -o dienstag
+clang ./mittwoch.c -o mittwoch
+clang ./donnerstag.c -o donnerstag
+clang ./freitag.c -o freitag
+clean
+echo "Finished Compilation Step."
